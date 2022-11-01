5adace0f6a036302f0843bd5500da81031ccdf6e
Berfin Özkök <berfnozkok@hotmail.com>
2022-11-02 01:21:56 +0300

    UserAccount

--- src/SignInSide.js
+++ src/SignInSide.js
@@ -1,30 +1,29 @@
 import * as React from "react";
-import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
 import CssBaseline from "@mui/material/CssBaseline";
 import TextField from "@mui/material/TextField";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import Checkbox from "@mui/material/Checkbox";
-//import Link from "@mui/material/Link";
-import { BrowserRouter, Route, Link } from "react-router-dom";
+import { Link, Routes } from "react-router-dom";
 import Paper from "@mui/material/Paper";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
+import UserSettings from "./UserSettings";
+import { useNavigate } from "react-router-dom";
 
 const theme = createTheme();
+
 
 export default function SignInSide() {
   const handleSubmit = (event) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
+    
     console.log({
       email: data.get("email"),
       password: data.get("password"),
     });
   };
-
   return (
     <ThemeProvider theme={theme}>
       <Grid container component="main" sx={{ height: "100vh" }}>
@@ -85,7 +84,7 @@ export default function SignInSide() {
                 id="password"
                 autoComplete="current-password"
               />
-              <Button
+              <Button onClick={() =>(UserSettings.js)}
                 type="submit"
                 fullWidth
                 variant="contained"
@@ -96,10 +95,10 @@ export default function SignInSide() {
               <Grid container>
                 <Grid item xs></Grid>
                 <Grid item>
-                  {/* <Link to="/signup" href="#" variant="body2">
-                    {"Don't have an account? Sign Up"}
-                  </Link> */}
-                  <Link to="/signup">Sıgn up</Link>
+                  {/*<Link to="/UserSettings" href="#" variant="body2">
+                    {"Account Settings  "}
+                  </Link>*/}
+                  <Link to="/signup">Sign up</Link>
                 </Grid>
               </Grid>
             </Box>
--- src/index.js
+++ src/index.js
@@ -4,7 +4,11 @@ import Layout from "./Layout";
 import NoPage from "./NoPage";
 import SignInSide from "./SignInSide";
 import Signup from "./Signup.js";
-import User from "./User";
+import UserSettings from "./UserSettings.js";
+import ChangeUsername from "./ChangeUsername.js";
+import ChangeEmail from "./ChangeEmail.js";
+import ChangePassword from "./ChangePassword.js";
+import DeleteAccount from "./DeleteAccount.js";
 
 export default function App() {
   return (
@@ -14,6 +18,12 @@ export default function App() {
           <Route index element={<SignInSide />} />
           <Route path="signup" element={<Signup />} />
           <Route path="*" element={<NoPage />} />
+          <Route path="usersettings" element={<UserSettings />} />
+          <Route path="changeusername" element={<ChangeUsername/>} />
+          <Route path="changeemail" element={<ChangeEmail/>} />
+          <Route path="changepassword" element={<ChangePassword/>} />
+          <Route path="deleteaccount" element={<DeleteAccount/>} />
+          <Route path="signinside" element={<SignInSide/>} />
         </Route>
       </Routes>
     </BrowserRouter>
