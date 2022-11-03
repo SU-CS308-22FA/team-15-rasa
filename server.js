d7ae3a00f4c08a55c906623cdad78a4f70d5061c
Umut Eren Örnek <ueren123@gmail.com>
2022-11-03 10:49:44 +0300

    Removed testing line

--- server.js
+++ server.js
@@ -8,9 +8,7 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 const MongoClient = mongodb.MongoClient;
-const uri =
-  process.env.USERS_DB_URI ||
-  "mongodb+srv://rasaReadWrite:T9AxKEQTnw5gYn0B@rasacluster.dxdtcrw.mongodb.net/users?retryWrites=true&w=majority";
+const uri = process.env.USERS_DB_URI;
 
 MongoClient.connect(uri)
   .catch((err) => {
@@ -26,7 +24,6 @@ MongoClient.connect(uri)
     if (process.env.NODE_ENV === "production") {
       app.use(express.static("client/build"));
     }
-    app.use(express.static("client/build"));
     app.listen(PORT, () => {
       console.log(`listening on port ${PORT}`);
     });
