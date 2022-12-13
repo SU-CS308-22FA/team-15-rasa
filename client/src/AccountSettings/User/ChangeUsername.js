5e5910134c8ebf2af115fd6e4eadd542d4d45c07
barisAytekin <barisaytekin5@gmail.com>
2022-12-14 00:18:45 +0300

    Initial commit

--- client/src/AccountSettings/User/ChangeUsername.js
+++ client/src/AccountSettings/User/ChangeUsername.js
@@ -3,12 +3,12 @@ import Button from "@mui/material/Button";
 import CssBaseline from "@mui/material/CssBaseline";
 import TextField from "@mui/material/TextField";
 //import Link from "@mui/material/Link";
-import {useLocation, useNavigate} from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
-import {createTheme, ThemeProvider} from "@mui/material/styles";
-import {AppBar, Container, Toolbar} from "@mui/material";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { AppBar, Container, Toolbar } from "@mui/material";
 import axios from "axios";
 
 const theme = createTheme();
@@ -24,8 +24,8 @@ export default function ChangeUsername() {
       return;
     }
     if (data.get("new_username") === location.state.username) {
-        alert("New username cannot be the same as the old username.");
-        return;
+      alert("New username cannot be the same as the old username.");
+      return;
     }
     axios
       .put("/api/v1", {
@@ -46,81 +46,79 @@ export default function ChangeUsername() {
       });
   };
   return (
-    <ThemeProvider theme={theme}>
-      <Grid container component="main" sx={{ height: "15vh" }}>
-        <CssBaseline />
-        <AppBar position="relative">
-          <Toolbar>
-            <Typography component="h1" variant="h5">
-              Account Settings
-            </Typography>
-          </Toolbar>
-        </AppBar>
-        <main>
-          <div>
-            <Container maxWidth="sm">
-              <Box
-                sx={{
-                  my: 8,
-                  mx: 4,
-                  display: "flex",
-                  flexDirection: "column",
-                  alignItems: "center",
-                }}
+    <Grid container component="main" sx={{ height: "15vh" }}>
+      <CssBaseline />
+      <AppBar position="relative">
+        <Toolbar>
+          <Typography component="h1" variant="h5">
+            Account Settings
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <main>
+        <div>
+          <Container maxWidth="sm">
+            <Box
+              sx={{
+                my: 8,
+                mx: 4,
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+              }}
+            >
+              <Typography component="h1" variant="h5">
+                Edit Username
+              </Typography>
+              <Typography
+                variant="h5"
+                align="center"
+                color="textSecondary"
+                paragraph
               >
-                <Typography component="h1" variant="h5">
-                  Edit Username
-                </Typography>
-                <Typography
-                  variant="h5"
-                  align="center"
-                  color="textSecondary"
-                  paragraph
+                You can change your username on this page.
+              </Typography>
+              <Box
+                component="form"
+                noValidate
+                onSubmit={handleSubmit}
+                sx={{ mt: 1 }}
+              >
+                <TextField
+                  margin="normal"
+                  required
+                  fullWidth
+                  id="new_username"
+                  label="New Username"
+                  name="new_username"
+                  autoComplete="new_username"
+                  autoFocus
+                />
+                <Button
+                  type="submit"
+                  fullWidth
+                  variant="contained"
+                  sx={{ mt: 3, mb: 2 }}
                 >
-                  You can change your username on this page.
-                </Typography>
-                <Box
-                  component="form"
-                  noValidate
-                  onSubmit={handleSubmit}
-                  sx={{ mt: 1 }}
-                >
-                  <TextField
-                    margin="normal"
-                    required
-                    fullWidth
-                    id="new_username"
-                    label="New Username"
-                    name="new_username"
-                    autoComplete="new_username"
-                    autoFocus
-                  />
-                  <Button
-                    type="submit"
-                    fullWidth
-                    variant="contained"
-                    sx={{ mt: 3, mb: 2 }}
-                  >
-                    Save Changes
-                  </Button>
-                  <Grid container>
-                    <Grid item xs></Grid>
-                    <Grid item>
-                      <Button
-                        onClick={() =>
-                          navigate("/accountsettings", {state: location.state})
-                        }
-                      >
-                        Return to account settings
-                      </Button>
-                    </Grid>
+                  Save Changes
+                </Button>
+                <Grid container>
+                  <Grid item xs></Grid>
+                  <Grid item>
+                    <Button
+                      onClick={() =>
+                        navigate("/accountsettings", { state: location.state })
+                      }
+                    >
+                      Return to account settings
+                    </Button>
                   </Grid>
-                </Box>
+                </Grid>
               </Box>
-            </Container>
-          </div>
-        </main>
-      </Grid>
-    </ThemeProvider>
+            </Box>
+          </Container>
+        </div>
+      </main>
+    </Grid>
   );
 }
--- client/src/AccountSettings/User/UserAccountSettings.js
+++ client/src/AccountSettings/User/UserAccountSettings.js
@@ -2,14 +2,22 @@ import * as React from "react";
 import CssBaseline from "@mui/material/CssBaseline";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
-import {useLocation, useNavigate} from "react-router-dom";
-import {AppBar, Container, Divider, List, ListItemButton, Toolbar,} from "@mui/material";
+import { useLocation, useNavigate } from "react-router-dom";
+import {
+  AppBar,
+  Container,
+  Divider,
+  List,
+  ListItemButton,
+  Toolbar,
+} from "@mui/material";
 import UserMenu from "../../UserMenu/UserMenu";
 import Box from "@mui/material/Box";
 
-export default function UserAccountSettings() {
+const UserAccountSettings = ({ state, stateChanger }) => {
   const location = useLocation();
   const navigate = useNavigate();
+
   return (
     <>
       <CssBaseline />
@@ -122,6 +130,20 @@ export default function UserAccountSettings() {
                     Home Page
                   </Button>
                 </ListItemButton>
+                <Divider light />
+                <ListItemButton>
+                  <Button
+                    type="submit"
+                    fullWidth
+                    variant="contained"
+                    sx={{ mt: 3, mb: 2 }}
+                    onClick={() => {
+                      stateChanger(!state);
+                    }}
+                  >
+                    Night/Day mode
+                  </Button>
+                </ListItemButton>
               </List>
             </div>
           </Container>
@@ -129,4 +151,5 @@ export default function UserAccountSettings() {
       </main>
     </>
   );
-}
+};
+export default UserAccountSettings;
