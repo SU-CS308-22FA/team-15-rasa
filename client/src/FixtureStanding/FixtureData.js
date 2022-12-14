35791c5c52c780bc6e6d345e8fa9864715273db0
Barış Aytekin <barisaytekin5@gmail.com>
2022-12-14 13:48:06 +0300

    Add profile page
    There were no controls on this page initially. In order to achieve
    functionality The user profile page was updated with 2 textboxes
    and an autocomplete.

--- client/src/FixtureStanding/FixtureData.js
+++ client/src/FixtureStanding/FixtureData.js
@@ -1,27 +1,25 @@
 import * as React from "react";
 import Typography from "@mui/material/Typography";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
-import '../CSS/Site.css';
-import {FixturesTable} from '../Components/FixturesTable';
+import "../CSS/Site.css";
+import { FixturesTable } from "../Components/FixturesTable";
 
 const theme = createTheme();
 
 function FixtureData() {
   return (
-    <ThemeProvider theme={theme}>
-      <div>
-        <Typography component="h1" variant="h5" className="ref-container">
-            Monthly Fixture
-          </Typography>
-          <div className="RefData">
-            <table>
-              <FixturesTable/> 
-            </table>  
-            <br></br>  
-            <br></br>                         
-          </div>
+    <div>
+      <Typography component="h1" variant="h5" className="ref-container">
+        Monthly Fixture
+      </Typography>
+      <div className="RefData">
+        <table>
+          <FixturesTable />
+        </table>
+        <br></br>
+        <br></br>
       </div>
-    </ThemeProvider>
+    </div>
   );
 }
 export default FixtureData;
