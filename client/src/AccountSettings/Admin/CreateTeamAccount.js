43ce206589b5564b0e1287cc31e1c03145953ebe
Barış Aytekin <barisaytekin5@gmail.com>
2022-12-14 13:48:06 +0300

    Add profile page
    There were no controls on this page initially. In order to achieve
    functionality The user profile page was updated with 2 textboxes
    and an autocomplete.

--- client/src/AccountSettings/Admin/CreateTeamAccount.js
+++ client/src/AccountSettings/Admin/CreateTeamAccount.js
@@ -4,119 +4,114 @@ import CssBaseline from "@mui/material/CssBaseline";
 import TextField from "@mui/material/TextField";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
-import {createTheme, ThemeProvider} from "@mui/material/styles";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 import axios from "axios";
-import {useNavigate} from "react-router-dom";
-import {AppBar, Toolbar} from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import { AppBar, Toolbar } from "@mui/material";
 
 const theme = createTheme();
 
 export default function CreateTeamAccount() {
-    const navigate = useNavigate();
-    const handleSubmit = (event) => {
+  const navigate = useNavigate();
+  const handleSubmit = (event) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
-      if (!data.get("email") || !data.get("password")) {
-          alert("Invalid email or password");
-          return;
-      }
+    if (!data.get("email") || !data.get("password")) {
+      alert("Invalid email or password");
+      return;
+    }
 
-      axios
-          .get("/api/v1", {
-              params: {
-                  _collection: "team_accounts",
-                  email: data.get("email"),
-              },
-          })
-          .catch((err) => {
+    axios
+      .get("/api/v1", {
+        params: {
+          _collection: "team_accounts",
+          email: data.get("email"),
+        },
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+      .then((res) => {
+        if (res && res.data.items.length !== 0) {
+          alert("Email already exists");
+          return;
+        }
+        if (res) {
+          axios
+            .post("/api/v1", {
+              _collection: "team_accounts",
+              username: data.get("username"),
+              email: data.get("email"),
+              password: data.get("password"),
+            })
+            .catch((err) => {
               console.log(err);
-          })
-          .then((res) => {
-              if (res && res.data.items.length !== 0) {
-                  alert("Email already exists");
-                  return;
+            })
+            .then((res) => {
+              if (res && res.status === 200) {
+                alert("Account created.");
+                navigate("/adminpanel");
+              } else {
+                alert("An error occurred while signing up, please try again.");
               }
-              if (res) {
-                  axios
-                      .post("/api/v1", {
-                          _collection: "team_accounts",
-                          username: data.get("username"),
-                          email: data.get("email"),
-                          password: data.get("password"),
-                      })
-                      .catch((err) => {
-                          console.log(err);
-                      })
-                      .then((res) => {
-                          if (res && res.status === 200) {
-                              alert("Account created.");
-                              navigate("/adminpanel");
-                          } else {
-                              alert("An error occurred while signing up, please try again.");
-                          }
-                      });
-              }
-          });
-    };
+            });
+        }
+      });
+  };
 
   return (
-    <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <AppBar position="relative">
+    <>
+      <CssBaseline />
+      <AppBar position="relative">
         <Toolbar>
           <Typography component="h1" variant="h5">
             Rasa Admin Panel
           </Typography>
         </Toolbar>
       </AppBar>
-          <Box
-            sx={{
-              my: 8,
-              mx: 4,
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-            }}
+      <Box
+        sx={{
+          my: 8,
+          mx: 4,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Typography component="h1" variant="h5">
+          Create Team Account
+        </Typography>
+        <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
           >
-            <Typography component="h1" variant="h5">
-              Create Team Account
-            </Typography>
-            <Box
-              component="form"
-              noValidate
-              onSubmit={handleSubmit}
-              sx={{ mt: 1 }}
-            >
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                id="email"
-                label="Email Address"
-                name="email"
-                autoComplete="email"
-                autoFocus
-              />
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                name="password"
-                label="Password"
-                type="password"
-                id="password"
-                autoComplete="current-password"
-              />
-              <Button
-                type="submit"
-                fullWidth
-                variant="contained"
-                sx={{ mt: 3, mb: 2 }}
-              >
-                Create
-              </Button>
-            </Box>
-          </Box>
-    </ThemeProvider>
+            Create
+          </Button>
+        </Box>
+      </Box>
+    </>
   );
 }
--- client/src/Auth/User/ForgotPassword.js
+++ client/src/Auth/User/ForgotPassword.js
@@ -2,12 +2,12 @@ import * as React from "react";
 import Button from "@mui/material/Button";
 import CssBaseline from "@mui/material/CssBaseline";
 import TextField from "@mui/material/TextField";
-import {Link, useNavigate} from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Paper from "@mui/material/Paper";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
-import {createTheme, ThemeProvider} from "@mui/material/styles";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 import axios from "axios";
 import emailjs from "emailjs-com";
 
@@ -19,119 +19,122 @@ export default function ForgotPassword() {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
     if (!data.get("email")) {
-        alert("Invalid email");
-        return;
+      alert("Invalid email");
+      return;
     }
     axios
       .get("/api/v1", {
         params: {
-            _collection: "users",
-            email: data.get("email"),
+          _collection: "users",
+          email: data.get("email"),
         },
       })
       .catch((err) => {
         console.log(err);
       })
       .then((res) => {
-        if (
-            res &&
-            (res.data.items.length === 0)
-        ) {
-            alert("Invalid email");
-            return;
+        if (res && res.data.items.length === 0) {
+          alert("Invalid email");
+          return;
         }
         if (res) {
-            var mailparams = {
-                to_name: res.data.items[0].username,
-                new_password: Math.floor((Math.random() * 9999) + 1000).toString(),
-                email: data.get("email")
-            }
-            console.log(mailparams.new_password);
+          var mailparams = {
+            to_name: res.data.items[0].username,
+            new_password: Math.floor(Math.random() * 9999 + 1000).toString(),
+            email: data.get("email"),
+          };
+          console.log(mailparams.new_password);
 
-            axios
-                .put("/api/v1", {
-                    _collection: "users",
-                    _id:  res.data.items[0]._id,
-                    password: mailparams.new_password,
-                });
+          axios.put("/api/v1", {
+            _collection: "users",
+            _id: res.data.items[0]._id,
+            password: mailparams.new_password,
+          });
 
-            emailjs.send('service_9jnl1hv', 'template_6tlwdfg', mailparams, '-MSMiMIn7nh6SMA04')
-                .then((result) => {
-                    console.log(result.text);
-                }, (error) => {
-                    console.log(error.text);
-                    alert(error);
-                });
-            alert("Check your e-mail.");
-            navigate("/");
+          emailjs
+            .send(
+              "service_9jnl1hv",
+              "template_6tlwdfg",
+              mailparams,
+              "-MSMiMIn7nh6SMA04"
+            )
+            .then(
+              (result) => {
+                console.log(result.text);
+              },
+              (error) => {
+                console.log(error.text);
+                alert(error);
+              }
+            );
+          alert("Check your e-mail.");
+          navigate("/");
         } else {
-            alert("There was an error. Please try again.");
+          alert("There was an error. Please try again.");
         }
       });
   };
 
   return (
-    <ThemeProvider theme={theme}>
-      <Grid container component="main" sx={{ height: "100vh" }}>
-        <CssBaseline />
-        <Grid
-          item
-          xs={false}
-          sm={4}
-          md={7}
+    <Grid container component="main" sx={{ height: "100vh" }}>
+      <CssBaseline />
+      <Grid
+        item
+        xs={false}
+        sm={4}
+        md={7}
+        sx={{
+          backgroundImage:
+            "url(https://www.evrensel.net/upload/dosya/167821.jpg)",
+          backgroundRepeat: "no-repeat",
+          backgroundColor: (t) =>
+            t.palette.mode === "light"
+              ? t.palette.grey[50]
+              : t.palette.grey[900],
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+      />
+      <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+        <Box
           sx={{
-            backgroundImage:
-              "url(https://www.evrensel.net/upload/dosya/167821.jpg)",
-            backgroundRepeat: "no-repeat",
-            backgroundColor: (t) =>
-              t.palette.mode === "light"
-                ? t.palette.grey[50]
-                : t.palette.grey[900],
-            backgroundSize: "cover",
-            backgroundPosition: "center",
+            my: 8,
+            mx: 4,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
           }}
-        />
-        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+        >
+          <Typography component="h1" variant="h5">
+            Enter your e-mail
+          </Typography>
           <Box
-            sx={{
-              my: 8,
-              mx: 4,
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-            }}
+            component="form"
+            noValidate
+            onSubmit={handleSubmit}
+            sx={{ mt: 1 }}
           >
-            <Typography component="h1" variant="h5">
-              Enter your e-mail
-            </Typography>
-            <Box
-              component="form"
-              noValidate
-              onSubmit={handleSubmit}
-              sx={{ mt: 1 }}
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
             >
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                id="email"
-                label="Email Address"
-                name="email"
-                autoComplete="email"
-                autoFocus
-              />
-              <Button
-                type="submit"
-                fullWidth
-                variant="contained"
-                sx={{ mt: 3, mb: 2 }}
-              >
-                Send
-              </Button>
-            </Box>
+              Send
+            </Button>
           </Box>
-        </Grid>
+        </Box>
       </Grid>
-    </ThemeProvider>
+    </Grid>
   );
 }
